3f01b0c38b9a808c2774155034b6b89e964977b8
Daniil Alekseev <55713843+Dapucla@users.noreply.github.com>
2024-08-21 00:25:19 +0300

    TGA-2: main_menu.dart: main menu screen added

---
+++ lib/main_menu.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainMenuScreen extends StatelessWidget {
+  const MainMenuScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Menu'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Main Menu!'),
+      ),
+    );
+  }
+}
