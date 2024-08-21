c9a16e970bb0423d68147d1a078d5805213b86c4
Daniil Alekseev <55713843+Dapucla@users.noreply.github.com>
2024-08-22 00:34:09 +0300

    TGA-3: chat_screen.dart: chat screen added

---
+++ lib/ui/chat_screen.dart
@@ -0,0 +1,16 @@
+// lib/screens/chat_screen.dart
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello, Flutter!'),
+        ),
+      ),
+    );
+  }
+}
