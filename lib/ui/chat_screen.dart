c92c4b0898fbae936a229734b324a1da2654df7b
Daniil Alekseev <55713843+Dapucla@users.noreply.github.com>
2024-08-23 18:33:21 +0300

    TGA-3: chat_screen.dart: chat refactoring and ui updated

--- lib/ui/chat_screen.dart
+++ lib/ui/chat_screen.dart
@@ -4,10 +4,10 @@ import 'package:tark_gpt_app/blocs/chat_cubit.dart';
 import 'package:tark_gpt_app/util/context_extensions.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'common_widgets/texts.dart';
 import 'common_widgets/my_app_bar.dart';
+import 'common_widgets/chat.dart';
+import 'common_widgets/texts.dart';
 import 'ui_constants.dart';
-import 'common_widgets/chat_input.dart';
 
 class ChatScreen extends StatefulWidget {
   final String? chatTitle;
@@ -22,6 +22,8 @@ class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _controller = TextEditingController();
   List<Map<String, String>> _messages = [];
   bool _showInitialMessage = true;
+  bool _isLoading = false;
+  bool _showRegenerateButton = false;
 
   @override
   void initState() {
@@ -41,12 +43,12 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
-        final title = widget.chatTitle ?? 'Chat ${DateTime.now().millisecondsSinceEpoch}';
+        final title =
+            widget.chatTitle ?? 'Chat ${DateTime.now().millisecondsSinceEpoch}';
         _saveChatHistory(title);
         Navigator.of(context).pop(title);
         return false;
       },
-
       child: Scaffold(
         appBar: MyAppBar(
           onTap: () {
@@ -66,61 +68,35 @@ class _ChatScreenState extends State<ChatScreen> {
           child: Column(
             children: [
               Expanded(
-                child: Stack(
-                  children: [
-                    // ListView.builder should take up available space
-                    ListView.builder(
-                      itemCount: _messages.length,
-                      itemBuilder: (context, index) {
-                        final message = _messages[index];
-                        final isUserMessage = message.containsKey('user');
-
-                        return ListTile(
-                          title: Align(
-                            alignment: isUserMessage
-                                ? Alignment.centerRight
-                                : Alignment.centerLeft,
-                            child: Container(
-                              padding: AppPadding.allNormal,
-                              decoration: BoxDecoration(
-                                color: isUserMessage ? context.greenAccent : context.cardBackground,
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              child: Text(
-                                isUserMessage ? message['user']! : message['bot']!,
-                                softWrap: true,
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                    if (_showInitialMessage)
-                      const Center(
-                        child: Texts(
-                          "Ask anything, get your answer",
-                          fontSize: AppSize.fontNormal,
-                          fontWeight: FontWeight.w600,
-                          isCenter: true,
-                        ),
-                      ),
-                  ],
+                child: BlocConsumer<ChatCubit, ChatState>(
+                  listener: (context, state) {
+                    if (state is ChatLoading) {
+                      setState(() {
+                        _isLoading = true;
+                        _showRegenerateButton = false;
+                        _messages.add({'loading': ''});
+                      });
+                    } else if (state is ChatSuccess) {
+                      setState(() {
+                        _isLoading = false;
+                        _messages.removeWhere(
+                            (message) => message.containsKey('loading'));
+                        _messages.add({'bot': state.response});
+                        _showRegenerateButton = true;
+                      });
+                    }
+                  },
+                  builder: (context, state) {
+                    return ChatUI(
+                      messages: _messages,
+                      showInitialMessage: _showInitialMessage,
+                      controller: _controller,
+                      sendMessage: _sendMessage,
+                      onRegenerate: _regenerateResponse,
+                      showRegenerateButton: _showRegenerateButton,
+                    );
+                  },
                 ),
-              ),
-              BlocConsumer<ChatCubit, ChatState>(
-                listener: (context, state) {
-                  if (state is ChatSuccess) {
-                    setState(() {
-                      _messages.add({'bot': state.response});
-                    });
-                  }
-                },
-                builder: (context, state) {
-                  return ChatInputField(
-                    controller: _controller,
-                    onSend: _sendMessage,
-                  );
-                },
               ),
             ],
           ),
@@ -132,18 +108,29 @@ class _ChatScreenState extends State<ChatScreen> {
   void _loadChatHistory(String chatTitle) async {
     final prefs = await SharedPreferences.getInstance();
     final chatHistory = prefs.getStringList(chatTitle);
-    if (chatHistory != null) {
+    if (chatHistory != null && chatHistory.isNotEmpty) {
       setState(() {
-        _messages = chatHistory
-            .map((message) => {'bot': message})
-            .toList();
+        _messages = chatHistory.map((message) {
+          final isUser = message.startsWith('user:');
+          return {isUser ? 'user' : 'bot': message.substring(4)};
+        }).toList();
+        _showInitialMessage = false;
       });
     }
   }
 
   void _saveChatHistory(String chatTitle) async {
     final prefs = await SharedPreferences.getInstance();
-    final chatHistory = _messages.map((message) => message['bot'] ?? '').toList();
+    final chatHistory = _messages.map((message) {
+      final userMessage = message['user'];
+      final botMessage = message['bot'];
+      if (userMessage != null) {
+        return 'user:$userMessage';
+      } else if (botMessage != null) {
+        return 'bot:$botMessage';
+      }
+      return '';
+    }).toList();
     prefs.setStringList(chatTitle, chatHistory);
   }
 
@@ -152,12 +139,23 @@ class _ChatScreenState extends State<ChatScreen> {
       setState(() {
         _messages.add({'user': _controller.text});
         _showInitialMessage = false;
+        _showRegenerateButton = false;
       });
 
       final userInput = _controller.text;
       _controller.clear();
 
       context.read<ChatCubit>().sendMessage(userInput);
+    }
+  }
+
+  void _regenerateResponse() {
+    if (_messages.isNotEmpty) {
+      final lastUserMessage =
+          _messages.lastWhere((message) => message.containsKey('user'))['user'];
+      if (lastUserMessage != null) {
+        context.read<ChatCubit>().sendMessage(lastUserMessage);
+      }
     }
   }
 }
